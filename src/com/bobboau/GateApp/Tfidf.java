5a532d1a5c91ddaff5ba686e43885e850835a93f
bobboau <bobboau@gmail.com>
2013-11-25 19:31:25 -0500

    added tf idf stub class

--- src/com/bobboau/GateApp/Tfidf.java
+++ src/com/bobboau/GateApp/Tfidf.java
@@ -3,10 +3,23 @@
  */
 package com.bobboau.GateApp;
 
+import gate.Corpus;
+
+import java.util.List;
+
 /**
  * @author bobboau
  *
  */
 public class Tfidf {
+	public void setCorpus(Corpus corpus){
+	}
 
+	public List<String> getTerms(){
+		return null;
+	}
+	
+	public double getScore(String term){
+		return 0.0;
+	}
 }
