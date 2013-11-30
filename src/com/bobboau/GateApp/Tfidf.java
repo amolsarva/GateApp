37a935a6f5cf90fbc939d1d05fcea3198f3e77af
bobboau <bobboau@gmail.com>
2013-11-30 00:56:54 -0500

    made the tfidf interface a bit more sane and useful

--- src/com/bobboau/GateApp/Tfidf.java
+++ src/com/bobboau/GateApp/Tfidf.java
@@ -12,14 +12,46 @@ import java.util.List;
  *
  */
 public class Tfidf {
+	/**
+	 * set a new corpus
+	 * @param corpus
+	 */
 	public void setCorpus(Corpus corpus){
 	}
 
+	/**
+	 * get a list of allterms in all documents
+	 * @return
+	 */
 	public List<String> getTerms(){
 		return null;
 	}
+
+	/**
+	 * get an unsorted list of all terms in the specified document
+	 * @param doc_idx which document we want the terms for in no particular order
+	 * @return
+	 */
+	public List<String> getTerms(int doc_idx){
+		return null;
+	}
+
+	/**
+	 * get a sorted list of all terms in the specified document, sorted in  order from highest score to lowest
+	 * @param doc_idx which document we want the terms for in no particular order
+	 * @return
+	 */
+	public List<String> getTermsOrdered(int doc_idx){
+		return null;
+	}
 	
-	public double getScore(String term){
+	/**
+	 * get the score of the given term in he context of the given document
+	 * @param term the term we want a scrote for
+	 * @param doc_idx which document we want the score calculated with respect to
+	 * @return
+	 */
+	public double getScore(String term, int doc_idx){
 		return 0.0;
 	}
 }
